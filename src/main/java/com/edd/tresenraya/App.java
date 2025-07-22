79625e7d5cd96ce9b76e45ae2d2121539af2df99
AlexitoSnow <ganieves@espol.edu.ec>
2025-07-21 22:41:01 -0500

    feat(router): add launch router to main

--- src/main/java/com/edd/tresenraya/App.java
+++ src/main/java/com/edd/tresenraya/App.java
@@ -1,34 +1,28 @@
 package com.edd.tresenraya;
 
+import com.edd.tresenraya.config.LoggerConfig;
+import com.edd.tresenraya.config.router.AppRouter;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class App extends Application {
 
-    private static Scene scene;
-
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+        //stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/assets/images/icon.png"))));
+        AppRouter.initStage(stage);
     }
 
     public static void main(String[] args) {
+        LoggerConfig.configure();
+
         launch();
     }
 
---
+++ src/main/java/com/edd/tresenraya/config/constants/Constants.java
@@ -0,0 +1,8 @@
+package com.edd.tresenraya.config.constants;
+
+public class Constants {
+
+    private Constants() {}
+
+    public static final String APP_NAME = "3 en Raya";
+}
