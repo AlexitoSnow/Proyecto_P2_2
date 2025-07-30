b5c298470b59438387e208735a9439dcfa522272
AlexitoSnow <ganieves@espol.edu.ec>
2025-07-29 23:52:55 -0500

    refactor: rename to follow conventions

--- src/main/java/com/edd/tresenraya/core/Box.java
+++ src/main/java/com/edd/tresenraya/core/Box.java
@@ -1,10 +1,9 @@
 package com.edd.tresenraya.core;
 
-public class box {
+public class Box {
 
     private char symbol;
     public boolean isFree;
-
 
     public void setSymbol(char symbol) {
         this.symbol = symbol;
