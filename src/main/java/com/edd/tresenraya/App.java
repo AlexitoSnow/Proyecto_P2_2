5a64c25bb23a8e54eb9e895b1e2792609b1a8ebc
AlexitoSnow <ganieves@espol.edu.ec>
2025-07-30 23:58:06 -0500

    refactor: remove unused imports

--- src/main/java/com/edd/tresenraya/App.java
+++ src/main/java/com/edd/tresenraya/App.java
@@ -3,14 +3,9 @@ package com.edd.tresenraya;
 import com.edd.tresenraya.config.LoggerConfig;
 import com.edd.tresenraya.config.router.AppRouter;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class App extends Application {
 
