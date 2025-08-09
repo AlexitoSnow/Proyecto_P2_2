ec80e82b357c6e7feed64e6b9f236708422e85c3
AlexitoSnow <ganieves@espol.edu.ec>
2025-08-09 12:46:18 -0500

    fix: add file constant

--- src/main/java/com/edd/tresenraya/config/constants/Constants.java
+++ src/main/java/com/edd/tresenraya/config/constants/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     private Constants() {}
 
     public static final String APP_NAME = "3 en Raya";
+    public static final String GAME_HISTORY_FILE = "game_history.csv";
 }
